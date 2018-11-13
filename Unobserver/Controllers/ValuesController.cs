8576bf898b370a7a6a9c9f4b0a850c4cdd45d0f1
Giorgi Chakhidze <g.chakhidze@altasoft.ge>
2018-11-13 17:55:57 +0400

    Initial commit

---
+++ Unobserver/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿namespace Unobserver.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(4000);
+
+                _logger.LogInformation("[WARNING] About to throw an unobserved exception.");
+
+                throw new InvalidOperationException("This is an unobserved exception. " +
+                    "The ONLY place you can see this is in TaskScheduler.UnobservedTaskException handler " +
+                    "and ONLY after this task gets garbage-collected!!!");
+            });
+
+            _logger.LogInformation("[SUCCESS] GET api/values");
+
+            return new[] { "value1", "value2" };
+        }
+
+        // Invoke this action to force GC collection; otherwise you'll have to wait for a loooooooong time
+        //
+        // GET api/values/gccollect
+        [HttpGet("gccollect")]
+        public ActionResult<string> GCCollect()
+        {
+            // Collects all GC generations up to MaxGeneration
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, blocking: true, compacting: false);
+
+            return "Garbage collected successfully.";
+        }
+    }
+}
---
+++ Unobserver/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Unobserver
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(config =>
+            {
+                config.AddConsole(options =>
+                {
+                    options.DisableColors = false;
+                    options.IncludeScopes = true;
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        // The ONLY place to observe unobservable Task exceptions is this method:
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            try
+            {
+                if (e.Exception is AggregateException agex)
+                {
+                    agex.Handle(ex =>
+                    {
+                        Console.Error.WriteLine(ex.ToString());
+                        Console.Error.WriteLine();
+                        return true;
+                    });
+                }
+                else
+                {
+                    Console.Error.WriteLine(e.Exception.ToString());
+                    Console.Error.WriteLine();
+                }
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+}
